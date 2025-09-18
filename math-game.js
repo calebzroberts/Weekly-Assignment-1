6aafcc94f452150b4a9dfb487a104d03fd94f0ce
calebzroberts <caleb.roberts798@gmail.com>
2025-09-18 07:59:21 -0400

    Designed level 2, where all numbers are guaranteed random and it can't divide by 0.

--- math-game.js
+++ math-game.js
@@ -361,10 +361,19 @@ var problemNumber = Number(1);
 var score = Number(0);
 var lives = Number(3);
 
+var firstNum;
+var secondNum;
+var operatorIndex;
+
+var lastFirstNum;
+var lastSecondNum;
+var lastOperatorIndex;
+
 var gameMode;
 
 console.log(`Welcome to the math game of wonders!`);
 console.log(`You have 2 modes, 20 questions, and 3-out.`);
+console.log(`Which do you want? \n Enter 1 for 20 questions \n Enter 2 for 3-out`);
 
 
 SelectGameMode();
@@ -372,7 +381,7 @@ SelectGameMode();
 //selects if 20 questions or if 3 out
 function SelectGameMode() 
 {
-    gameMode = Number(prompt(`Which do you want? \n Enter 1 for 20 questions \n Enter 2 for 3-out \n`));
+    gameMode = Number(prompt(`Game mode: `));
 
     if (gameMode === 1) 
     {
@@ -418,33 +427,37 @@ function Mode3Out()
 //selects a random operator and then calls the appropriate function with that operator
 function SelectRandomOperator()
 {
-    var OperatorIndex = RandomInt(1, 5);
+    //keeps selecting new operator until it is different than the last operator
+    operatorIndex = GetOperator();
 
-    var firstNum = RandomInt(1, 10);
-    var secondNum = RandomInt(1, 10);
+
+    firstNum = GetFirstNum();
+    secondNum = GetLastNum();
+
+    CheckDivideZero();
     
 
-    if (OperatorIndex === 1)
+    if (operatorIndex === 1)
     {
         RandomSum(firstNum, secondNum);
     }
 
-    else if (OperatorIndex === 2)
+    else if (operatorIndex === 2)
     {
         RandomSubtract(firstNum, secondNum);
     }
 
-    else if (OperatorIndex === 3)
+    else if (operatorIndex === 3)
     {
         RandomMultiply(firstNum, secondNum);
     }
 
-    else if (OperatorIndex === 4)
+    else if (operatorIndex === 4)
     {
         RandomDivide(firstNum, secondNum);
     }
 
-    else if (OperatorIndex === 5)
+    else if (operatorIndex === 5)
     {
         RandomModulo(firstNum, secondNum);
     }
@@ -461,7 +474,7 @@ function RandomSum(firstNum, secondNum)
     //prints problem number first
     console.log(`${problemNumber})`);
 
-    var input = prompt(`${firstNum} + ${secondNum} = ? \n`);
+    var input = prompt(`${firstNum} + ${secondNum} = ?`);
 
     //if player can skip (20 questions mode) and does skip, then skip
     if (gameMode === 1 && input === "skip")
@@ -494,7 +507,7 @@ function RandomSubtract(firstNum, secondNum)
     //prints problem number first
     console.log(`${problemNumber})`);
 
-    var input = prompt(`${firstNum} - ${secondNum} = ? \n`);
+    var input = prompt(`${firstNum} - ${secondNum} = ? `);
 
     //if player can skip (20 questions mode) and does skip, then skip
     if (gameMode === 1 && input === "skip")
@@ -527,7 +540,7 @@ function RandomMultiply(firstNum, secondNum)
     //prints problem number first
     console.log(`${problemNumber})`);
 
-    var input = prompt(`${firstNum} * ${secondNum} = ? \n`);
+    var input = prompt(`${firstNum} * ${secondNum} = ? `);
 
     //if player can skip (20 questions mode) and does skip, then skip
     if (gameMode === 1 && input === "skip")
@@ -560,7 +573,7 @@ function RandomDivide(firstNum, secondNum)
     //prints problem number first
     console.log(`${problemNumber})`);
 
-    var input = prompt(`${firstNum} / ${secondNum} = ? \n`);
+    var input = prompt(`${firstNum} / ${secondNum} = ? `);
 
     //if player can skip (20 questions mode) and does skip, then skip
     if (gameMode === 1 && input === "skip")
@@ -593,7 +606,7 @@ function RandomModulo(firstNum, secondNum)
     //prints problem number first
     console.log(`${problemNumber})`);
 
-    var input = prompt(`${firstNum} % ${secondNum} = ? \n`);
+    var input = prompt(`${firstNum} % ${secondNum} = ? `);
 
     //if player can skip (20 questions mode) and does skip, then skip
     if (gameMode === 1 && input === "skip")
@@ -705,5 +718,56 @@ function CheckPrintLives()
     if(gameMode === 2)
     {
         console.log(`You have ${lives} lives left.`);
+    }
+}
+
+//gets the operator to use in the equation
+function GetOperator()
+{
+    while(lastOperatorIndex === operatorIndex)
+    {
+        operatorIndex = RandomInt(0, 5);
+    }
+
+    lastOperatorIndex = operatorIndex;
+    return operatorIndex;
+    
+}
+
+//gets the first number to use in the equation
+function GetFirstNum()
+{
+    while(lastFirstNum === firstNum)
+    {
+        firstNum = RandomInt(0, 10);
+    }
+
+    lastFirstNum = firstNum;
+    return firstNum;
+    
+}
+
+//gets the last number to use in the equation
+function GetLastNum()
+{
+    while(lastSecondNum === secondNum)
+    {
+        secondNum = RandomInt(0, 10);
+    }
+
+    lastSecondNum = secondNum;
+    return secondNum;
+
+}
+
+//checks to make sure its not asking the user to divide by 0
+function CheckDivideZero()
+{
+    if(operatorIndex === 2)
+    {
+        while(secondNum === 0)
+        {
+            scoreNum = GetSecondNum();
+        }
     }
 }
